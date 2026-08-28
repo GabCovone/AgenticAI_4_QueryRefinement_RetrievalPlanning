f8c46e01c936c7cbb59313b06f26817bb501f888
GabCovone <covonegabriel@gmail.com>
2026-08-28 19:38:18 +0200

    bug fix

--- graph.py
+++ graph.py
@@ -19,8 +19,8 @@ class GraphState(TypedDict):
 from huggingface_hub import hf_hub_download
 from langchain_community.chat_models import ChatLlamaCpp 
 
-REPO_ID = "bartowski/InternLM2.5-20B-Chat-GGUF" 
-FILENAME = "InternLM2.5-20B-Chat-Q4_K_M.gguf" 
+REPO_ID = "bartowski/gemma-2-27b-it-GGUF" 
+FILENAME = "gemma-2-27b-it-Q4_K_M.gguf" 
 LOCAL_DIR = "./models"
 LOCAL_PATH = os.path.join(LOCAL_DIR, FILENAME)
 
